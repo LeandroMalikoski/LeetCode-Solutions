62b70cbdaf1ea251537d16de8ddab94468d00935
Leandro Malikoski <leandroborgesmalikoski@gmail.com>
2025-05-30 16:52:51 -0300

    problem solved: AddTwoNumbers

---
+++ src/solutions/medium/AddTwoNumbers.java
@@ -0,0 +1,78 @@
+package solutions.medium;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+
+        ListNode l1 = new ListNode(9);
+        l1.next = new ListNode(9);
+        l1.next.next = new ListNode(9);
+
+        ListNode l2 = new ListNode(7);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(1);
+
+        Solution solution = new Solution();
+        ListNode result = solution.addTwNumbers(l1, l2);
+
+        System.out.println(result);
+        printList(result);
+
+
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val);
+            if (node.next != null) System.out.print(" -> ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+class Solution {
+    public ListNode addTwNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummy.next;
+
+    }
+}
